c48d1860e918163104dfc61ac05652d9e150cac8
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-10-07 01:40:02 +0900

    feat(cli): implement interface selection and fetch network interface details
    - Added command-line interface (CLI) for specifying the network interface.
    - Fetch and display the selected interface's details using `pnet::datalink`.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // get interface
+    let interface_name = cli.interface;
+    let interfaces = pnet::datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .find(|iface| iface.name == interface_name)
+        .expect("Interface not found");
+    println!("{:?}", interface);
+
+    // create ARP packet
+
+    // send ARP packet
+
+    // recieve ARP packet
+}
+
+#[derive(Debug, Parser)]
+/// A tool for scanning devices in a local network by sending ARP requests.
+struct Cli {
+    #[arg(short, long, required = true)]
+    /// The network interface to use.
+    interface: String,
 }
